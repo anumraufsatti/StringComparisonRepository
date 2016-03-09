cb3fc18272bc736093f92a4a3a43e8d087946073
1234github <irumraufsatti@hotmail.com>
2016-03-09 11:37:57 +0500

    String comparison package

---
+++ stringcomparison.go
@@ -0,0 +1,11 @@
+// Package String.Comparison is used for string comparison
+
+package StringComparison
+
+// Equals function print the bar
+func Equals(string1 string, string2 string) bool {
+	if string1 == string2 {
+		return true
+	}
+	return false
+}
